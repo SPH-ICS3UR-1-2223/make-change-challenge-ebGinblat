a7ba2b5e090bd112203917d95b52ce8d68370fe9
ebGinblat <elias.ginblat@stu.ocsb.ca>
2022-10-18 14:30:05 -0400

    couldnt finish in time

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,15 @@ public class Main {
 		// TODO Auto-generated method stub
 		Scanner in = new Scanner(System.in);
 		//Your challenge is to read in a price
+		System.out.println("Enter a price");
+		double price = in.nextDouble();
 		//Read in the amount paid
+		System.out.println("How much was paid?");
+		double paid = in.nextDouble();
 		//Print out the amount of change
-		//Break the change into all denominations: 
+		double change = paid - price;
+		System.out.println("Your change is $"+change);
+		//Break the change into all denominations:
 		//$100,$50,$20,$10,$5,$2,$1,$0.25,$0.10,$0.05
 		//As a bonus, make your project round to the nearest $0.05 like actual stores.
 		
