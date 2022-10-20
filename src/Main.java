9c7071c6d25766efe3a45d95687a37803833ee77
ebGinblat <elias.ginblat@stu.ocsb.ca>
2022-10-20 13:28:44 -0400

    MF DOOM >

--- src/Main.java
+++ src/Main.java
@@ -27,24 +27,24 @@ public class Main {
 		int num10 = (int)( change/10);
 		System.out.println("You get "+num10+" x $10");
 		change = change-num10*10;
-		int num100 = (int)( change/100);
-		System.out.println("You get "+num100+" x $100");
-		change = change-num100*100;
-		int num100 = (int)( change/100);
-		System.out.println("You get "+num100+" x $100");
-		change = change-num100*100;
-		int num100 = (int)( change/100);
-		System.out.println("You get "+num100+" x $100");
-		change = change-num100*100;
-		int num100 = (int)( change/100);
-		System.out.println("You get "+num100+" x $100");
-		change = change-num100*100;
-		int num100 = (int)( change/100);
-		System.out.println("You get "+num100+" x $100");
-		change = change-num100*100;
-		int num100 = (int)( change/100);
-		System.out.println("You get "+num100+" x $100");
-		change = change-num100*100;
+		int num5 = (int)( change/5);
+		System.out.println("You get "+num5+" x $5");
+		change = change-num5*5;
+		int num2 = (int)( change/2);
+		System.out.println("You get "+num2+" x $2");
+		change = change-num2*2;
+		int num1 = (int)( change/1);
+		System.out.println("You get "+num1+" x $1");
+		change = change-num1*1;
+		int num0_25 = (int)( change/0.25);
+		System.out.println("You get "+num0_25+" x $0.25");
+		change = change-num0_25*0.25;
+		int num0_10 = (int)( change/0.10);
+		System.out.println("You get "+num100+" x $0.10");
+		change = change-num0_10*0.10;
+		int num0_05 = (int)( change/0.05);
+		System.out.println("You get "+num0_05+" x $0.05");
+		change = change-num0_05*0.05;
 		//$100,$50,$20,$10,$5,$2,$1,$0.25,$0.10,$0.05
 		//As a bonus, make your project round to the nearest $0.05 like actual stores.
 		
